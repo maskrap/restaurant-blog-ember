d792018de23b6ba007dc635e3f6c33a8de7d91cd
Marty Fitchen and Sky Rousse <sky@>
2016-08-16 11:27:47 -0700

    add working files with the abillity to delete articles from the index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
   },
+
+  actions: {
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
