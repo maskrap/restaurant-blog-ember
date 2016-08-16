8377643b1ca8bafa455366219ec049e0a610cf64
Marty Fitchen and Sky Rousse <sky@>
2016-08-16 13:53:30 -0700

    add working files for an admin page to be able to delete and add new articles

--- app/components/article-tile.js
+++ app/components/article-tile.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    delete(article) {
-      if (confirm('Are you sure you want to delete this article?')) {
-        this.sendAction('destroyArticle', article);
-      }
-    }
-  }
+  
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,18 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
-  },
-
-  actions: {
-    save(params) {
-      var newArticle = this.store.createRecord('article', params);
-      newArticle.save();
-      this.transitionTo('index');
-    },
-
-    destroyArticle(article) {
-      article.destroyRecord();
-      this.transitionTo('index');
-    }
   }
 });
